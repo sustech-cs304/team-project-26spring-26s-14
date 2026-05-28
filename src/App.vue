3545dfa2a0ab7892c02f285e59c356ca5b88e8af
SEATheStArs12 <12011714@mail.sustech.edu.cn>
2026-05-28 09:44:24 +0800

    feat: setup application entry, Supabase client and global styles

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useUserStore } from './stores/user'
+
+const userStore = useUserStore()
+
+onMounted(() => {
+  userStore.init()
+})
+</script>
+
+<<template>
+  <router-view />
+</template>
